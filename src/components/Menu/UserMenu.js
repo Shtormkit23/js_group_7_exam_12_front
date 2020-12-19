397647e4d147779d950dd33541f26a9589f689e1
maksimus <gorlovmaks23@gmail.com>
2020-12-19 16:48:40 +0600

    Добавила меню для авторизированного и анонимного пользователя

---
+++ src/components/Menu/UserMenu.js
@@ -0,0 +1,81 @@
+import React from "react";
+import {Button, ListItemAvatar} from "@material-ui/core";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import {makeStyles} from "@material-ui/core/styles";
+import {NavLink} from "react-router-dom";
+import {logoutUser} from "../../store/actions/usersActions";
+import {useDispatch} from "react-redux";
+import Avatar from "@material-ui/core/Avatar";
+import imageNotAvailable from "../../download.png";
+import {apiURL} from "../../constants";
+
+
+
+const useStyles = makeStyles(theme => ({
+    dropDownBtn: {
+        color: theme.palette.common.white
+    },
+    link: {
+        textDecoration: "none",
+        color: "black"
+    }
+}));
+
+const UserMenu = ({user}) => {
+    const dispatch = useDispatch();
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const logout = () => {
+        dispatch(logoutUser());
+    };
+
+    let avatarImage = imageNotAvailable;
+    if (user.avatarImage) {
+        avatarImage = apiURL + "/uploads/" + user.avatarImage;
+    }
+    if (user.facebookImage) {
+        avatarImage = user.facebookImage;
+    }
+
+    return (
+        <>
+            <ListItemAvatar>
+                <Avatar src={avatarImage} />
+            </ListItemAvatar>
+            <Button
+                id="userMenu"
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                className={classes.dropDownBtn}
+            >
+                Hello, {user.displayName ? user.displayName : user.username}
+            </Button>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                {
+                    user &&
+                    <MenuItem><NavLink className={classes.link} to="/my_gallery">My Gallery</NavLink></MenuItem>
+                }
+                <MenuItem onClick={logout}>Logout</MenuItem>
+            </Menu>
+        </>
+    );
+};
+
+export default UserMenu;
